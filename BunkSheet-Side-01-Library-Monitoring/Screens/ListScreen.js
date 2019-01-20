67ef6f794697790ca72d883a0be1569c7fd1e10f
Param Jain <param_jain@icloud.com>
2019-01-21 01:42:18 +0530

    App Completed ;)

--- BunkSheet-Side-01-Library-Monitoring/Screens/ListScreen.js
+++ BunkSheet-Side-01-Library-Monitoring/Screens/ListScreen.js
@@ -36,7 +36,7 @@ class ListScreen extends Component {
     }
 
     makeRemoteRequest = () => {
-        const url = `https://collegebuddy.pythonanywhere.com/api/book/100`;
+        const url = `https://mighty-hollows-23016.herokuapp.com/cc/getAllBooks`;
         this.setState({ loading: true });
     
         fetch(url)
@@ -122,29 +122,18 @@ class ListScreen extends Component {
       );
     }
 
-    bookDetailModal = (item) => {
-      this.setState({bookSelected: item}, () =>{
-        this.setModalVisible(true);
-      });
-    }
-
-    setModalVisible(visible) {
-      this.setState({modalVisible: visible});
-    }
-
     renderSeparator = () => {
       return (
         <View
           style={{
             height: 1,
-            width: '86%',
             backgroundColor: '#CED0CE',
-            marginLeft: '14%',
+            marginLeft: '3%',
+            marginRight: '3%',
           }}
         />
       );
     };
-
 
   renderList = () => {
     return (
@@ -153,96 +142,48 @@ class ListScreen extends Component {
             keyboardShouldPersistTaps='always'
             data={this.state.data}
             renderItem={({ item }) => (
-              <ListItem
+            <ListItem
               roundAvatar
-              title={item.Title}
+              title={item.data.volumeInfo.title}
               titleStyle = {{fontWeight: "bold"}}
-              subtitle={`A: ${item.Author}  P: ${item.Publisher}`}
-              leftAvatar={{ source: { uri: "http://books.google.com/books/content?id=_ojXNuzgHRcC&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE730gA8gykyn3eW-2UcqAait5rm7mkY0fxMIYsgNqe7rMLL1N1Lem_aEPK4CjW_o-gWHKcV2yQw6EtiyUhmZkNa2Mp4GgfRpnLNjq7lgHu_Nfwj1TaZZBkmqxR2coVrJ9BWmydUK&source=gbs_api" } }} //uri:item.image
+              subtitle={`${item.data.volumeInfo.authors}`}
               containerStyle={{ borderBottomWidth: 0 }}
-              chevronColor="white"
               chevron
-              onPress={() => this.bookDetailModal(item)}
+              chevronColor='#CED0CE'
               />
             )}
-            keyExtractor={item => item.id.toString()}
+            keyExtractor={item => item._id.toString()}
             ItemSeparatorComponent={this.renderSeparator}
             //ListHeaderComponent={this.renderHeader}
           />
 
-          <Modal
-            animationType={'fade'}
-            transparent={true}
-            onRequestClose={() => this.setModalVisible(false)}
-            visible={this.state.modalVisible}>
-
-            <View style={styles.popupOverlay}>
-              <View style={styles.popup}>
-                <View style={styles.popupContent}>
-                  <ScrollView contentContainerStyle={styles.modalInfo}>
-                      <Image style={styles.image} source={{uri: this.state.bookSelected.Image}}/>
-                      <Text style={styles.name}>{this.state.bookSelected.Title}</Text> 
-                      <Text style={styles.author}>Author: {this.state.bookSelected.Author}</Text>
-                      <Text style={styles.publisher}>Publisher: {this.state.bookSelected.Publisher}</Text>
-                      <Text style={styles.about}>"Discover the beautiful science of flowers! Through full-color photos and simple, easy-to-follow text, this nonfiction book introduces emergent readers to the basics of botany, including information on how flowers grow, along with their uses. All Pebble Plus books align with national and state standards and are designed to help new readers read independently, making them the perfect choice for every child."</Text>
-                      <View style={{flexDirection: "row"}}>
-                        <Text style={styles.pageCount}>Page Count: {this.state.bookSelected.pc}</Text>
-                        {this.state.bookSelected.AC > 0 ? <Text style={styles.available}>Can be Issued</Text>: <Text style={styles.notAvailable}>Already Issued</Text> }
-                      </View>
-                  </ScrollView>
-                </View>
-                <View style={styles.popupButtons}>
-                <LinearGradient
-                  colors={['#FF9800', '#FB8C00', '#EF6C00']}
-                  style={styles.btnClose}
-                  >
-                    <TouchableOpacity onPress={() => {this.setModalVisible(false) }}>
-                      <Text style={styles.txtClose}>Close</Text>
-                    </TouchableOpacity>
-                  </LinearGradient>
-                </View>
-              </View>
-            </View>
-          </Modal>
-
         </ScrollView>
     );
   }
-
-toNotificationScreen() {
-  this.props.navigation.navigate('libraryNotifications');
-}
-
-toBarCodeScannerScreen() {
-  this.props.navigation.navigate('issuance');
-}
 
   renderHeader = () => {
     return(
       <Header
         backgroundColor="#FF6D00"
         outerContainerStyles={{borderBottomWidth: 0.5, borderColor: '#000000'}}
-        centerContainerStyle={{paddingTop: 10}}
-        rightContainerStyle={{paddingTop: 10}}
-        leftContainerStyle={{margin: 10}}
-        centerComponent={{ text: 'BunkSheet', style: { color: '#fff',fontSize: 24, fontWeight: 'bold' } }}
+        centerComponent={{ text: 'BunkSheet', style: { color: '#fff',fontSize: 26, fontWeight: 'bold', marginBottom: 0, marginTop: 10 } }}
         //rightComponent={this.renderRightComponent()}
         //leftComponent={{ icon: 'barcode', type: 'font-awesome', color: '#fff', onPress: () => this.toBarCodeScannerScreen(), size: 30, underlayColor:'#64b5f6' }}
       />
     );
   }
     render() {
-        if (this.state.loading) {
+        if (this.state.loading){
             return (
-                <ActivityIndicator size='large'/>
+                <ActivityIndicator size='large' animating={this.state.loading}/>
             );
-          }
+        }
         return (
             <KeyboardAvoidingView style={styles.container} behavior="padding">
                 <StatusBar barStyle = "dark-content" hidden = {false} translucent = {true}/>
                 <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                    <View style={styles.container}>
-                        
+                <View style={styles.container}>
+
                         {this.renderHeader()}
 
                         <View style={styles.sectionStyle}>
@@ -250,7 +191,6 @@ toBarCodeScannerScreen() {
                           {this.modifySearchBar()}
                           {this.crossIconFunctionality()}
                         </View>
-
                         {this.renderList()}
 
                     </View>
